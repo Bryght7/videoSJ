4c7648056deaff0444ac90b58aedc8495e72340f
Bryan Lee <leebryan.perso@gmail.com>
2022-04-12 00:24:08 +0200

    🐛 Fix bug change bg gradient on value change from parent

--- src/renderer/components/VideoSeeker.tsx
+++ src/renderer/components/VideoSeeker.tsx
@@ -1,4 +1,4 @@
-import { SyntheticEvent } from 'react';
+import { SyntheticEvent, useEffect } from 'react';
 
 type Props = {
   played: number;
@@ -6,19 +6,31 @@ type Props = {
   onChange: (event: SyntheticEvent) => void;
 };
 
+// Input range theme
+const getBackground = (value: number, max: number) =>
+  `linear-gradient(to right, #4f46e5 0%, #4f46e5 ${
+    (value * 100) / max
+  }%, #e5e7eb ${(value * 100) / max}%, #e5e7eb 100%)`;
+
 const VideoSeeker = ({ played, totalDuration, onChange }: Props) => {
   const handleOnInput = (event: SyntheticEvent) => {
     const target = event.target as HTMLInputElement;
     const element = document.getElementById('inputVideoSeeker');
     if (target && element) {
-      // Input range theme
-      element.style.background = `linear-gradient(to right, #4f46e5 0%, #4f46e5 ${
-        (parseInt(target.value, 10) * 100) / parseInt(target.max, 10)
-      }%, #e5e7eb ${
-        (parseInt(target.value, 10) * 100) / parseInt(target.max, 10)
-      }%, #e5e7eb 100%)`;
+      element.style.background = getBackground(
+        parseInt(target.value, 10),
+        parseInt(target.max, 10)
+      );
     }
   };
+
+  useEffect(() => {
+    const element = document.getElementById('inputVideoSeeker');
+    if (element) {
+      element.style.background = getBackground(played, totalDuration);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [played]);
 
   return (
     <input
--- src/renderer/components/VolumeSlider.tsx
+++ src/renderer/components/VolumeSlider.tsx
@@ -1,4 +1,4 @@
-import { SyntheticEvent } from 'react';
+import { SyntheticEvent, useEffect } from 'react';
 import VolumeIcon from './VolumeIcon';
 
 type Props = {
@@ -9,6 +9,12 @@ type Props = {
   onMute: (prevValue: number) => void;
   onUnmute: () => void;
 };
+
+// Input range theme
+const getBackground = (value: number, max: number) =>
+  `linear-gradient(to right, #4f46e5 0%, #4f46e5 ${
+    (value * 100) / max
+  }%, #e5e7eb ${(value * 100) / max}%, #e5e7eb 100%)`;
 
 const VolumeSlider = ({
   min = 0,
@@ -23,13 +29,20 @@ const VolumeSlider = ({
     const element = document.getElementById('inputVolumeSlider');
     if (target && element) {
       // Input range theme
-      element.style.background = `linear-gradient(to right, #4f46e5 0%, #4f46e5 ${
-        (parseInt(target.value, 10) * 100) / parseInt(target.max, 10)
-      }%, #e5e7eb ${
-        (parseInt(target.value, 10) * 100) / parseInt(target.max, 10)
-      }%, #e5e7eb 100%)`;
+      element.style.background = getBackground(
+        parseInt(target.value, 10),
+        parseInt(target.max, 10)
+      );
     }
   };
+
+  useEffect(() => {
+    const element = document.getElementById('inputVolumeSlider');
+    if (element) {
+      element.style.background = getBackground(value, max);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
 
   return (
     <div className="flex items-center">
